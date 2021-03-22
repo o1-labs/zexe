944156daa1c65b4b5aed467931287743c9d2a8d5
Izaak Meckler <ihmeckler@gmail.coom>
2021-03-21 18:26:00 -0700

    add clone, copy

--- groupmap/src/lib.rs
+++ groupmap/src/lib.rs
@@ -9,6 +9,7 @@ pub trait GroupMap<F> {
     fn batch_to_group_x(&self, ts: Vec<F>) -> Vec<[F; 3]>;
 }
 
+#[derive(Copy, Clone)]
 pub struct BWParameters<G: SWModelParameters> {
     u: G::BaseField,
     fu: G::BaseField,
