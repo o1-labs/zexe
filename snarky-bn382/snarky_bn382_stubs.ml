3c84f3f5798cf067e4fa94f3545faa42548c0af7
David Wong <davidwong.crypto@gmail.com>
2021-06-03 18:00:29 -0700

    [snarky] removing the snarky curve

--- snarky-bn382/snarky_bn382_stubs.ml
+++
@@ -1,9 +0,0 @@
-module Ignored = struct
-  (* Statically link the symbols at the library level. *)
-  external name : unit -> unit = "zexe_bn382_fp_size_in_bits"
-
-  let name () = ()
-end
-[@@warning "-32"]
-
-let linkme () = ()
